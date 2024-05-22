e61a5b1b25db82d1c15709df8ff355851277b737
Inferencer <121839197+Inferencer@users.noreply.github.com>
2024-05-22 16:33:13 +0100

    New Blend Feature
    Added auto mask feature which is activated by default to remove box around mouth

--- app.py
+++ app.py
@@ -24,24 +24,22 @@ def compute_crop_radius_stats(video_file):
     print(f"Done: Crop radius = {most_common}")
     return most_common
 
-def process_files(source_video, driving_audio, custom_crop_radius=None, generate_same_length_video=False, ref_index_1=None, ref_index_2=None, ref_index_3=None, ref_index_4=None, ref_index_5=None, activate_custom_frames=False):
-    # Adjust reference indices before sending them to the inference script
+def process_files(source_video, driving_audio, custom_crop_radius=None, auto_mask=True, ref_index_1=None, ref_index_2=None, ref_index_3=None, ref_index_4=None, ref_index_5=None, activate_custom_frames=False):
     ref_indices = [index for index in [ref_index_1, ref_index_2, ref_index_3, ref_index_4, ref_index_5] if index is not None]
     if custom_crop_radius is None or custom_crop_radius == 0:
-        # Add 5 to each index to ensure no negative indexing happens in inference.py when it subtracts 5
         ref_indices = [index + 5 for index in ref_indices]
     ref_indices_str = ','.join(map(str, ref_indices)) if len(ref_indices) == 5 else ""
 
     if not driving_audio:
         print("Please upload audio first.")
-        return "", "Error: Audio file is required.", None
+        return "", "Error: Audio file is required."
 
     pretrained_model_path = "./asserts/pretrained_lipsick.pth"
     deepspeech_model_path = "./asserts/output_graph.pb"
     res_video_dir = "./asserts/inference_result"
 
     base_name = os.path.splitext(os.path.basename(source_video))[0]
-    output_video_name = f"{base_name}_LIPSICK.mp4"
+    output_video_name = f"{base_name}_lipsick.mp4" if not auto_mask else "LipSick_Blend.mp4"
     output_video_path = os.path.join(res_video_dir, output_video_name)
     output_video_path = get_versioned_filename(output_video_path)
 
@@ -52,37 +50,30 @@ def process_files(source_video, driving_audio, custom_crop_radius=None, generate
         '--pretrained_lipsick_path', pretrained_model_path,
         '--deepspeech_model_path', deepspeech_model_path,
         '--res_video_dir', res_video_dir,
-        '--custom_reference_frames', ref_indices_str  # Pass the comma-separated string of indices
+        '--custom_reference_frames', ref_indices_str
     ]
 
-    # Only add the custom_crop_radius if it's a positive number
     if custom_crop_radius is not None:
         cmd.extend(['--custom_crop_radius', str(custom_crop_radius)])
 
     if activate_custom_frames:
         cmd.append('--activate_custom_frames')
 
-    if generate_same_length_video:
-        cmd.append('--generate_same_length_video')
+    if auto_mask:
+        cmd.append('--auto_mask')
 
     try:
-        subprocess.run(cmd, check=True)  # Display output from the Python script
-        print("Creating low resolution browser playable video")
+        subprocess.run(cmd, check=True)
         print("Complete")
-        print("High quality version saved in ./asserts/inference_results")
+        print("Result saved in ./asserts/inference_results")
         print("Thank you for using LipSick for your Lip-Sync needs.")
         
-        # Determine the path of the same-length extra video
-        same_length_video_path = output_video_path.replace('_facial_dubbing.mp4', '_samelength.mp4')
+        # Return the path of the processed video
+        return "", output_video_path
         
-        if generate_same_length_video:
-            return "", output_video_path, same_length_video_path
-        else:
-            return "", output_video_path
     except subprocess.CalledProcessError as e:
         print(f"An error occurred: {e}")
-        return "An error occurred during processing. Please check the console log.", "", None
-
+        return "An error occurred during processing. Please check the console log.", ""
 
 with gr.Blocks(css=".input_number { width: 80px; }") as iface:
     gr.Markdown("### 🤢 LIPSICK 🤮\nUpload your video and driving audio to Lipsync.")
@@ -92,7 +83,7 @@ with gr.Blocks(css=".input_number { width: 80px; }") as iface:
         driving_audio = gr.File(label="Upload .Wav Audio File", type="filepath", file_types=["wav"])
         
         with gr.Accordion("Advanced Options", open=False):
-            generate_same_length_video = gr.Checkbox(label="Generate same-length extra video without lipsyncing for masking - this will be saved in the folder ./asserts/inference_results", value=False)
+            auto_mask = gr.Checkbox(label="Auto Mask", value=True)
             face_tracker_options = ['dlib', 'opencv', 'openface', 'sfd', 'blazeface']
             face_tracker = gr.CheckboxGroup(
                 label="Face Tracker",
@@ -103,10 +94,8 @@ with gr.Blocks(css=".input_number { width: 80px; }") as iface:
             )
             gr.Markdown("___")  # Visual separator for layout
             
-            # Checkbox to activate custom reference frames
             activate_custom_frames = gr.Checkbox(label="Activate Custom Reference Frames", value=False)
             
-            # Reference indices input
             gr.Markdown("""
             Enter custom reference frames for example 0 1 2 3 4 5. 
             All boxes must be filled with a value and the checkbox above must be activated. 
@@ -140,12 +129,10 @@ with gr.Blocks(css=".input_number { width: 80px; }") as iface:
         process_btn = gr.Button("Process Video")
         output_video = gr.Video(label="Processed Video", show_label=False)
         status_text = gr.Textbox(visible=False)  # Reintroduce a hidden Textbox for stability
-
-        # Process files with ref indices and checkboxes
+        # Adjust outputs to include only the processed video
         process_btn.click(
             fn=process_files,
-            inputs=[source_video, driving_audio, custom_crop_radius, generate_same_length_video, ref_index_1, ref_index_2, ref_index_3, ref_index_4, ref_index_5, activate_custom_frames],
+            inputs=[source_video, driving_audio, custom_crop_radius, auto_mask, ref_index_1, ref_index_2, ref_index_3, ref_index_4, ref_index_5, activate_custom_frames],
             outputs=[status_text, output_video]
         )
-
     iface.launch(inbrowser=True)
